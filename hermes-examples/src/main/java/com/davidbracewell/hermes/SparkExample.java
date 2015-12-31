fb2ecabd9ceb9865124ae06eaa6f8a749facab4e
David Bracewell <david@my_email.com>
2015-12-31 11:25:14 -0600

    Updates

--- hermes-examples/src/main/java/com/davidbracewell/hermes/SparkExample.java
+++ hermes-examples/src/main/java/com/davidbracewell/hermes/SparkExample.java
@@ -11,7 +11,6 @@ import java.io.Serializable;
 
 public class SparkExample implements Serializable {
   private static final long serialVersionUID = 1L;
-  private static final Logger log = Logger.getLogger(SparkExample.class);
 
   public static void main(String[] args) throws Exception {
     Config.initialize("SparkExample");
@@ -24,16 +23,15 @@ public class SparkExample implements Serializable {
     Corpus corpus = Corpus.builder()
       .distributed()
       .format(DocumentFormats.PLAIN_TEXT_OPL)
-      //You can substitute the file for one you have. Here I am using a 10,000 sentence corpus fron news articles with
+      //You can substitute the file for one you have. Here I am using a 10,000 sentence corpus from news articles with
       // one sentence (treated as a document) per line.
       .source(Resources.fromFile("/data/corpora/en/eng_news_2005_10K-text/eng_news_2005_10K-sentences.txt")).build()
       .annotate(Types.TOKEN);
 
-    //Calculate term frequencies for the corpus. Not we are saying we want lemmatized versions, but have not
-    //ran the lemma annotator, instead it will just return the lowercase version of the content.
+    //Calculate term frequencies for the corpus. Note we are saying we want lemmatized versions, but have not
+    //run the lemma annotator, instead it will just return the lowercase version of the content.
     Counter<String> counts = corpus.termFrequencies(true);
     counts.entries().forEach(entry -> System.out.println(entry.getKey() + " => " + entry.getValue()));
-
   }
 
 }
