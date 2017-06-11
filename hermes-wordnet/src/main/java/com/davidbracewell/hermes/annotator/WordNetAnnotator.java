d78b6482eb1c4b2b9aff5f693cdf8af23b6a36eb
David Bracewell <david@my_email.com>
2017-06-11 12:32:53 -0500

    Wordnet fix

--- hermes-wordnet/src/main/java/com/davidbracewell/hermes/annotator/WordNetAnnotator.java
+++ hermes-wordnet/src/main/java/com/davidbracewell/hermes/annotator/WordNetAnnotator.java
@@ -4,6 +4,7 @@ import com.davidbracewell.collection.Trie;
 import com.davidbracewell.hermes.*;
 import com.davidbracewell.hermes.morphology.Lemmatizer;
 import com.davidbracewell.hermes.morphology.Lemmatizers;
+import com.davidbracewell.hermes.wordnet.Sense;
 import com.davidbracewell.hermes.wordnet.WordNet;
 
 import java.util.Collections;
@@ -16,115 +17,118 @@ import java.util.stream.Collectors;
  * @author David B. Bracewell
  */
 public class WordNetAnnotator extends SentenceLevelAnnotator {
-    private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L;
 
-    @Override
-    public void annotate(Annotation sentence) {
-        List<Annotation> tokens = sentence.tokens();
-        Document document = sentence.document();
-        Lemmatizer lemmatizer = Lemmatizers.getLemmatizer(sentence.getLanguage());
-        for (int i = 0; i < tokens.size(); ) {
-            Annotation token = tokens.get(i);
-            final Trie<String> lemmas = lemmatizer.allPossibleLemmasAndPrefixes(tokens
-                                                                                    .get(i)
-                                                                                    .toString(), POS.ANY);
+   @Override
+   public void annotate(Annotation sentence) {
+      List<Annotation> tokens = sentence.tokens();
+      Document document = sentence.document();
+      Lemmatizer lemmatizer = Lemmatizers.getLemmatizer(sentence.getLanguage());
+      for (int i = 0; i < tokens.size(); ) {
+         Annotation token = tokens.get(i);
+         final Trie<String> lemmas = lemmatizer.allPossibleLemmasAndPrefixes(tokens
+                                                                                .get(i)
+                                                                                .toString(), POS.ANY);
 
-            if (lemmas.size() > 0) {
+         if (lemmas.size() > 0) {
 
-                HString bestMatch = null;
+            HString bestMatch = null;
 
-                if (lemmas.size() == 1 && lemmatizer.canLemmatize(token.toString(), token.getPOS())) {
+            if (lemmas.size() == 1 && lemmatizer.canLemmatize(token.toString(), token.getPOS())) {
 
-                    bestMatch = token;
+               bestMatch = token;
 
-                } else if (lemmas.size() > 1) {
+            } else if (lemmas.size() > 1) {
 
-                    Set<String> working = getAllLemmas(token, lemmatizer)
-                                              .stream()
-                                              .filter(s -> lemmas.containsKey(s) || lemmas
-                                                                                        .prefix(s + " ")
-                                                                                        .size() > 0)
-                                              .collect(Collectors.toSet());
+               Set<String> working = getAllLemmas(token, lemmatizer)
+                                        .stream()
+                                        .filter(s -> lemmas.containsKey(s) || lemmas
+                                                                                 .prefix(s + " ")
+                                                                                 .size() > 0)
+                                        .collect(Collectors.toSet());
 
-                    if (lemmatizer.canLemmatize(token.toString(), token.getPOS())) {
-                        bestMatch = token;
-                    }
+               if (lemmatizer.canLemmatize(token.toString(), token.getPOS())) {
+                  bestMatch = token;
+               }
 
-                    int startChar = token.start();
-                    int end = i + 1;
-                    while (end < tokens.size()) {
-                        boolean matched = false;
-                        token = tokens.get(end);
-                        Set<String> nextSet = new HashSet<>();
-                        for (String previous : working) {
-                            for (String next : getAllLemmas(token, lemmatizer)) {
-                                String phrase = previous + " " + next;
-                                if (lemmas.containsKey(phrase)) {
-                                    nextSet.add(phrase);
-                                    matched = true;
-                                } else if (lemmas
-                                               .prefix(phrase)
-                                               .size() > 0) {
-                                    nextSet.add(phrase);
-                                }
-                            }
+               int startChar = token.start();
+               int end = i + 1;
+               while (end < tokens.size()) {
+                  boolean matched = false;
+                  token = tokens.get(end);
+                  Set<String> nextSet = new HashSet<>();
+                  for (String previous : working) {
+                     for (String next : getAllLemmas(token, lemmatizer)) {
+                        String phrase = previous + " " + next;
+                        if (lemmas.containsKey(phrase)) {
+                           nextSet.add(phrase);
+                           matched = true;
+                        } else if (lemmas
+                                      .prefix(phrase)
+                                      .size() > 0) {
+                           nextSet.add(phrase);
                         }
-                        working = nextSet;
-                        HString span = document.substring(startChar, token.end());
-                        if (matched) {
-                            bestMatch = span;
-                        }
-                        if (nextSet.isEmpty()) {
-                            break;
-                        }
-                        end++;
-                    }
-                }
-
-
-                if (bestMatch == null) {
-                    i++;
-                } else {
-                    createAnnotation(document, bestMatch);
-                    i += bestMatch.tokenLength();
-                }
-
-
-            } else {
-                i++;
+                     }
+                  }
+                  working = nextSet;
+                  HString span = document.substring(startChar, token.end());
+                  if (matched) {
+                     bestMatch = span;
+                  }
+                  if (nextSet.isEmpty()) {
+                     break;
+                  }
+                  end++;
+               }
             }
-        }
-    }
 
-    private Annotation createAnnotation(Document document, Span span) {
-        Annotation annotation = document
-                                    .annotationBuilder()
-                                    .type(Types.WORD_SENSE)
-                                    .bounds(span)
-                                    .createAttached();
-        annotation.put(Types.LEMMA, WordNet
-                                        .getInstance()
-                                        .getSenses(annotation.toString(), POS.forText(annotation),
-                                                   document.getLanguage())
-                                        .get(0)
-                                        .getLemma());
-        return annotation;
-    }
 
-    @Override
-    protected Set<AnnotatableType> furtherRequires() {
-        return Collections.singleton(Types.PART_OF_SPEECH);
-    }
+            if (bestMatch == null) {
+               i++;
+            } else {
+               createAnnotation(document, bestMatch);
+               i += bestMatch.tokenLength();
+            }
 
-    private Set<String> getAllLemmas(HString hString, Lemmatizer lemmatizer) {
-        Set<String> all = new HashSet<>(lemmatizer.allPossibleLemmas(hString.toString(), POS.ANY));
-        all.add(hString.toLowerCase());
-        return all;
-    }
 
-    @Override
-    public Set<AnnotatableType> satisfies() {
-        return Collections.singleton(Types.WORD_SENSE);
-    }
+         } else {
+            i++;
+         }
+      }
+   }
+
+   private Annotation createAnnotation(Document document, Span span) {
+      Annotation annotation = document
+                                 .annotationBuilder()
+                                 .type(Types.WORD_SENSE)
+                                 .bounds(span)
+                                 .createAttached();
+      List<Sense> senses = WordNet.getInstance().getSenses(annotation.toString(),
+                                                           POS.forText(annotation),
+                                                           document.getLanguage());
+      if (senses.isEmpty()) {
+         senses = WordNet.getInstance().getSenses(annotation.toString(),
+                                                  POS.ANY,
+                                                  document.getLanguage());
+      }
+      annotation.put(Types.SENSE, senses);
+      return annotation;
+   }
+
+   @Override
+   protected Set<AnnotatableType> furtherRequires() {
+      return Collections.singleton(Types.PART_OF_SPEECH);
+   }
+
+   private Set<String> getAllLemmas(HString hString, Lemmatizer lemmatizer) {
+      Set<String> all = new HashSet<>(lemmatizer.allPossibleLemmas(hString.toString(), POS.ANY));
+      all.add(hString.toLowerCase());
+      return all;
+   }
+
+   @Override
+   public Set<AnnotatableType> satisfies() {
+      return Collections.singleton(Types.WORD_SENSE);
+   }
 
 }// END OF WordNetAnnotator
