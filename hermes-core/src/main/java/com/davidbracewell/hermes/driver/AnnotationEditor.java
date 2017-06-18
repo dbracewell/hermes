5f03f4e9c8fe0ab22bdb03f3c4c749639d75da27
David Bracewell <david@my_email.com>
2017-06-17 20:32:17 -0500

    Fix TaggedFormat, updates to annotation editor

--- hermes-core/src/main/java/com/davidbracewell/hermes/driver/AnnotationEditor.java
+++ hermes-core/src/main/java/com/davidbracewell/hermes/driver/AnnotationEditor.java
@@ -23,7 +23,7 @@ import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyleContext;
 import java.awt.*;
-import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
@@ -156,6 +156,30 @@ public class AnnotationEditor extends SwingApplication {
       editorPane.setComponentPopupMenu(createEditorPopup());
       editorPane.addMouseListener(mouseClicked(this::syncEditorSelection));
       editorPane.addMouseListener(mousePressed(this::syncEditorSelection));
+
+      editorPane.addKeyListener(new KeyAdapter() {
+         @Override
+         public void keyReleased(KeyEvent e) {
+            if (e.isControlDown() && e.getKeyChar() == '+') {
+               Font font = editorPane.getFont();
+               if (font.getSize() < 24) {
+                  Font f2 = new Font(font.getName(), font.getStyle(), font.getSize() + 2);
+                  editorPane.setFont(f2);
+               }
+            } else if (e.isControlDown() && e.getKeyChar() == '-') {
+               Font font = editorPane.getFont();
+               if (font.getSize() > 12) {
+                  Font f2 = new Font(font.getName(), font.getStyle(), font.getSize() - 2);
+                  editorPane.setFont(f2);
+               }
+            }
+            super.keyPressed(e);
+         }
+      });
+
+
+      editorPane.getCaret().setVisible(true);
+
       return editorPane;
    }
 
@@ -183,42 +207,6 @@ public class AnnotationEditor extends SwingApplication {
       }));
 
       return editorPopup;
-   }
-
-   private void createToolbar() {
-      toolbarPanel = new JPanel();
-      toolbarPanel.setLayout(new WrapLayout());
-      add(toolbarPanel, BorderLayout.NORTH);
-      int i = 1;
-      for (Tag tag : validTypes) {
-         int adj = (i >= 10) ? (i - 9) : i;
-         String btnName = tag.name();
-         if (i < 20) {
-            btnName += "(" + (i >= 10 ? "a+" : "") + adj + ")";
-         }
-         JButton button = new JButton(btnName);
-         button.addActionListener(a -> addTag(tag));
-         toolbarPanel.add(button);
-         Action a = new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-               addTag(tag);
-            }
-         };
-         String code = "";
-         if (i > 10) {
-            code += " alt";
-         }
-         code += " pressed " + adj;
-         a.putValue(Action.ACCELERATOR_KEY,
-                    KeyStroke.getKeyStroke(code));
-         i++;
-         button.getActionMap().put("myaction-" + i, a);
-         button.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
-               .put((KeyStroke) a.getValue(Action.ACCELERATOR_KEY), "myaction-" + i);
-      }
-
-
    }
 
    private void deleteAnnotation() {
@@ -302,6 +290,7 @@ public class AnnotationEditor extends SwingApplication {
             }
          });
       editorPane.setCaretPosition(0);
+      editorPane.getCaret().setVisible(true);
    }
 
    @Override
@@ -360,17 +349,43 @@ public class AnnotationEditor extends SwingApplication {
       menuBar.add(menu);
       setJMenuBar(menuBar);
 
+      JMenu addAnnotation = new JMenu("Tag");
+      addAnnotation.setMnemonic(KeyEvent.VK_T);
+      menuBar.add(addAnnotation);
+      int i = 0;
+      for (Tag tag : validTypes) {
+         int adj = i % 10;
+         JMenuItem button = new JMenuItem(tag.name());
+         button.addActionListener(a -> addTag(tag));
+
+         addAnnotation.add(button);
+         String code = "";
+         if (i >= 30) {
+            code += "control alt";
+         } else if (i >= 20) {
+            code += "control";
+         } else if (i >= 10) {
+            code += " alt";
+         }
+         code += " pressed " + adj;
+         i++;
+         button.setAccelerator(KeyStroke.getKeyStroke(code));
+      }
+
       JSplitPane splitPane = new JSplitPane();
       splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
       splitPane.setDividerLocation(300);
       add(splitPane, BorderLayout.CENTER);
 
       splitPane.add(createAnnotationTable(), JSplitPane.BOTTOM);
-      splitPane.add(new JScrollPane(createEditor()), JSplitPane.TOP);
-      createToolbar();
+      JPanel editorPanel = new JPanel();
+      editorPanel.setLayout(new BorderLayout());
+      editorPanel.add(createEditor());
+      splitPane.add(new JScrollPane(editorPanel), JSplitPane.TOP);
 
       Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
       this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
+
 
    }
 
@@ -563,176 +578,6 @@ public class AnnotationEditor extends SwingApplication {
 
       public boolean spanHasAnnotation(int start, int end) {
          return annotations.overlapping(new Span(start, end)).size() > 0;
-      }
-   }
-
-   class WrapLayout extends FlowLayout {
-      private Dimension preferredLayoutSize;
-
-      /**
-       * Constructs a new <code>WrapLayout</code> with a left
-       * alignment and a default 5-unit horizontal and vertical gap.
-       */
-      public WrapLayout() {
-         super();
-      }
-
-      /**
-       * Constructs a new <code>FlowLayout</code> with the specified
-       * alignment and a default 5-unit horizontal and vertical gap.
-       * The value of the alignment argument must be one of
-       * <code>WrapLayout</code>, <code>WrapLayout</code>,
-       * or <code>WrapLayout</code>.
-       *
-       * @param align the alignment value
-       */
-      public WrapLayout(int align) {
-         super(align);
-      }
-
-      /**
-       * Creates a new flow layout manager with the indicated alignment
-       * and the indicated horizontal and vertical gaps.
-       * <p>
-       * The value of the alignment argument must be one of
-       * <code>WrapLayout</code>, <code>WrapLayout</code>,
-       * or <code>WrapLayout</code>.
-       *
-       * @param align the alignment value
-       * @param hgap  the horizontal gap between components
-       * @param vgap  the vertical gap between components
-       */
-      public WrapLayout(int align, int hgap, int vgap) {
-         super(align, hgap, vgap);
-      }
-
-      /**
-       * Returns the preferred dimensions for this layout given the
-       * <i>visible</i> components in the specified target container.
-       *
-       * @param target the component which needs to be laid out
-       * @return the preferred dimensions to lay out the subcomponents of the specified container
-       */
-      @Override
-      public Dimension preferredLayoutSize(Container target) {
-         return layoutSize(target, true);
-      }
-
-      /**
-       * Returns the minimum dimensions needed to layout the <i>visible</i>
-       * components contained in the specified target container.
-       *
-       * @param target the component which needs to be laid out
-       * @return the minimum dimensions to lay out the subcomponents of the specified container
-       */
-      @Override
-      public Dimension minimumLayoutSize(Container target) {
-         Dimension minimum = layoutSize(target, false);
-         minimum.width -= (getHgap() + 1);
-         return minimum;
-      }
-
-      /**
-       * Returns the minimum or preferred dimension needed to layout the target
-       * container.
-       *
-       * @param target    target to get layout size for
-       * @param preferred should preferred size be calculated
-       * @return the dimension to layout the target container
-       */
-      private Dimension layoutSize(Container target, boolean preferred) {
-         synchronized (target.getTreeLock()) {
-            //  Each row must fit with the width allocated to the containter.
-            //  When the container width = 0, the preferred width of the container
-            //  has not yet been calculated so lets ask for the maximum.
-
-            int targetWidth = target.getSize().width;
-            Container container = target;
-
-            while (container.getSize().width == 0 && container.getParent() != null) {
-               container = container.getParent();
-            }
-
-            targetWidth = container.getSize().width;
-
-            if (targetWidth == 0)
-               targetWidth = Integer.MAX_VALUE;
-
-            int hgap = getHgap();
-            int vgap = getVgap();
-            Insets insets = target.getInsets();
-            int horizontalInsetsAndGap = insets.left + insets.right + (hgap * 2);
-            int maxWidth = targetWidth - horizontalInsetsAndGap;
-
-            //  Fit components into the allowed width
-
-            Dimension dim = new Dimension(0, 0);
-            int rowWidth = 0;
-            int rowHeight = 0;
-
-            int nmembers = target.getComponentCount();
-
-            for (int i = 0; i < nmembers; i++) {
-               Component m = target.getComponent(i);
-
-               if (m.isVisible()) {
-                  Dimension d = preferred ? m.getPreferredSize() : m.getMinimumSize();
-
-                  //  Can't add the component to current row. Start a new row.
-
-                  if (rowWidth + d.width > maxWidth) {
-                     addRow(dim, rowWidth, rowHeight);
-                     rowWidth = 0;
-                     rowHeight = 0;
-                  }
-
-                  //  Add a horizontal gap for all components after the first
-
-                  if (rowWidth != 0) {
-                     rowWidth += hgap;
-                  }
-
-                  rowWidth += d.width;
-                  rowHeight = Math.max(rowHeight, d.height);
-               }
-            }
-
-            addRow(dim, rowWidth, rowHeight);
-
-            dim.width += horizontalInsetsAndGap;
-            dim.height += insets.top + insets.bottom + vgap * 2;
-
-            //	When using a scroll pane or the DecoratedLookAndFeel we need to
-            //  make sure the preferred size is less than the size of the
-            //  target containter so shrinking the container size works
-            //  correctly. Removing the horizontal gap is an easy way to do this.
-
-            Container scrollPane = SwingUtilities.getAncestorOfClass(JScrollPane.class, target);
-
-            if (scrollPane != null && target.isValid()) {
-               dim.width -= (hgap + 1);
-            }
-
-            return dim;
-         }
-      }
-
-      /*
-       *  A new row has been completed. Use the dimensions of this row
-       *  to update the preferred size for the container.
-       *
-       *  @param dim update the width and height when appropriate
-       *  @param rowWidth the width of the row to add
-       *  @param rowHeight the height of the row to add
-       */
-      private void addRow(Dimension dim, int rowWidth, int rowHeight) {
-         dim.width = Math.max(dim.width, rowWidth);
-
-         if (dim.height > 0) {
-            dim.height += getVgap();
-         }
-
-         dim.height += rowHeight;
       }
    }
 
